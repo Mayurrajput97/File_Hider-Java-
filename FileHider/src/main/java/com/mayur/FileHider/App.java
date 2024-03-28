148ac03f51b5eebe3df5400305251f3d68af8cd0
Mayurrajput97 <mayurrajput7827@gmail.com>
2024-03-28 23:47:24 +0530

    Initial commit

---
+++ FileHider/src/main/java/com/mayur/FileHider/App.java
@@ -0,0 +1,13 @@
+package com.mayur.FileHider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
