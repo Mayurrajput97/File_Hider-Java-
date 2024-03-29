f5febb882e7f112ea1d0f2bc3459eb3694a1ba3e
Mayurrajput97 <mayurrajput7827@gmail.com>
2024-03-29 10:49:26 +0530

    MyConnection Class

---
+++ FileHider/src/main/java/db/Myconnection.java
@@ -0,0 +1,35 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Myconnection {
+    public static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/filehider?useSSL=false", "root", "Mayur");
+
+        } catch (ClassNotFoundException | SQLException e) { // corrected typo from SQlException to SQLException
+            e.printStackTrace();
+        }
+        System.out.println("Connection is done successfully");
+        return connection;
+    }
+
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Myconnection.getConnection();
+    }
+}
